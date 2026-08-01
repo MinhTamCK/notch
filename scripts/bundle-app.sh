23a3e4298d4db8c2691a57a2b8cf2a34e3d15468
MinhTamCK <6836685+MinhTamCK@users.noreply.github.com>
2026-08-01 09:06:18 +0700

    Bump version to 0.4.0

--- scripts/bundle-app.sh
+++ scripts/bundle-app.sh
@@ -36,7 +36,7 @@ cat > "$BUNDLE/Contents/Info.plist" <<'EOF'
   <key>CFBundleDisplayName</key><string>Notch</string>
   <key>CFBundleIconFile</key><string>AppIcon</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>0.3.5</string>
+  <key>CFBundleShortVersionString</key><string>0.4.0</string>
   <key>CFBundleVersion</key><string>1</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
